29b8efbff2a1090d7d29fa6b99447c701874173f
LyviN <51911607+xLyviNx@users.noreply.github.com>
2024-01-20 19:38:51 +0100

    Update Pracownicy.java

--- src/main/java/com/synowiecsygut/klientbazy/Pracownicy.java
+++ src/main/java/com/synowiecsygut/klientbazy/Pracownicy.java
@@ -1,11 +1,15 @@
 package com.synowiecsygut.klientbazy;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
+import java.sql.CallableStatement;
+import java.sql.SQLException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class Pracownicy
 {
@@ -19,32 +23,57 @@ public class Pracownicy
     TextField input_wyna;
     @FXML
     TextField input_stano;
-
-
     @FXML
-    void dodaj()
-    {
-        System.out.println("Gruby");
-    }
+    void dodaj() {
+        String naz = input_naz.getText();
+        String imie = input_imie.getText();
+        String wynagrodzenieStr = input_wyna.getText();
+        String stanowisko = input_stano.getText();
 
+        if (naz.isEmpty() || imie.isEmpty() || wynagrodzenieStr.isEmpty() || stanowisko.isEmpty()) {
+            Utilities.showAlert("Błąd", "Wszystkie dane (oprócz ID) muszą być wypełnione!", Alert.AlertType.ERROR);
+            return;
+        }
+
+        try {
+            float wynagrodzenie = Float.parseFloat(wynagrodzenieStr);
+            try (DatabaseConnection db = new DatabaseConnection())
+            {
+                CallableStatement callableStatement = db.getConnection().prepareCall("{call DodajPracownika(?, ?, ?, ?)}");
+                callableStatement.setString(1, naz);
+                callableStatement.setString(2, imie);
+                callableStatement.setFloat(3, wynagrodzenie);
+                callableStatement.setString(4, stanowisko);
+
+                callableStatement.execute();
+                int rowCount = callableStatement.getUpdateCount();
+                if (rowCount > 0) {
+                    Utilities.showAlert("Informacja", "Dodano pracownika.", Alert.AlertType.INFORMATION);
+                } else {
+                    Utilities.showAlert("Błąd!", "Nie udało się dodać pracownika.", Alert.AlertType.ERROR);
+                }
+            } catch (SQLException e) {
+                Utilities.showAlert("Błąd!", "Błąd bazy danych!\n" + e.getLocalizedMessage(), Alert.AlertType.ERROR);
+                System.err.println(e.getLocalizedMessage());
+            }
+        } catch (NumberFormatException e) {
+            Utilities.showAlert("Błąd", "Wynagrodzenie musi być liczbą!", Alert.AlertType.ERROR);
+        }
+    }
     @FXML
     void usun()
     {
-        System.out.println("ty");
     }
 
     @FXML
     void edytuj()
     {
-        System.out.println("jebany");
     }
 
     @FXML
     void wys()
     {
-        System.out.println("zaklamany");
     }
-
     @FXML
     void wroc()
     {
