b9b215d599693d4e16c8144d08a6bada43881d61
Ryuu666 <ryuu666sup@gmail.com>
2024-01-20 18:35:53 +0100

    siurururu

--- src/main/java/com/synowiecsygut/klientbazy/MainWindow.java
+++ src/main/java/com/synowiecsygut/klientbazy/MainWindow.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class MainWindow extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 800);
         stage.setTitle("Klient Bazy Danych");
         stage.setScene(scene);
